a8e0b04c92ec6df6a5b287ab9863cb2b58410877
OlgaB <olga.bereznaja@gmail.com>
2020-06-05 10:34:09 +0100

    commit 01

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let money;
+let income = 25000;
+let addExpenses = 10000;
+let deposit = false;
+let mission = 150000;
+let period = 12;
+
+alert('Это- сообщение в Алерте.');
+
+console.log('Это- сообщение в Консоли.');
