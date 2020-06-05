126d9e94ee9be2022875553aa36c7ff3fb75a152
OlgaB <olga.bereznaja@gmail.com>
2020-06-05 20:13:15 +0100

    Lesson-02 done

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 'use strict';
 
-let money;
-let income = 25000;
-let addExpenses = 10000;
+let money = 25000;
+let income = 'Основная работа, фриланс.';
+let addExpenses = 'Счета за квартиру. Счета за телефон. Покупки.';
 let deposit = false;
 let mission = 150000;
 let period = 12;
+let budgetDay = money / 30;
 
-alert('Это- сообщение в Алерте.');
+console.log('Money: ', typeof money);
+console.log('Income: ', typeof income);
+console.log('Deposit: ', typeof deposit);
 
-console.log('Это- сообщение в Консоли.');
+console.log('AddExpenses length: ', addExpenses.length);
+
+console.log('Период равен: ', period, 'ти месяцам/месяцев. \nЦель - заработать ', mission, ' рублей.');
+
+console.log('AddExpenses: ');
+console.log(addExpenses.toLowerCase().split('. '));
+
+console.log('Дневной бюджет: ', Math.round(budgetDay));
+
