85b1459d1a8c0c5cdc940076727f6d0d9c512c95
OlgaB <olga.bereznaja@gmail.com>
2020-06-23 21:51:22 +0100

    ready to cleanUp

--- script.js
+++ script.js
@@ -32,28 +32,83 @@ let appData = {
   addExpenses: [],
   deposit: false,
   mission: 150000,
-  period: 12,
+  period: 3,
   budgetDay: 0,
-  budgetMonth: 0,
+  budgetMonth: 0,//Сколько зарабатываем за месяц- высчитан из getBudget
   expensesMonth: 0,
+  percentDeposit: '',
+  moneyDeposit: '',
   //
   asking: function () {
 
-    let addExpenses = prompt('“Перечислите возможные статьи расходов через запятую”', 'Подарки родственникам, путешествия, оплата за курсы.');
-    appData.addExpenses = addExpenses.toLowerCase().split(', ');
-    appData.deposit = confirm('“Есть ли у вас депозит в банке?”');
+    //Урок 8- вопрос про дополнительный заработок
+    if (confirm('Есть ли у вас дополнительный источник заработка?')) {
 
-    /*
-    7) Перенести цикл из метода getExpensesMonth в метод asking, и переписать цикл таким образом чтобы результат записывался в объект  appData.expenses
-в формате:
-expenses:
-    “ответ на первый вопрос” : “ответ на второй вопрос”,
-    “ответ на первый вопрос” : “ответ на второй вопрос”
+      let itemIncome;
+
+      do {
+        itemIncome = prompt('Какой именно?', 'Садоводство');
+        //Продолжаем спрашивать, пока возвращается полноценное число ил нулл или пустая строка.
+      } while (
+        (itemIncome === '' || itemIncome === null || isNumber(itemIncome) || itemIncome.trim() === '' || isNumber(itemIncome.trim()))
+      );
+
+      let cashIncome;
+
+      do {
+        cashIncome = prompt('Сколько в месяц вы на этом зарабатываете?', '10000');//NUMBER?
+      } while (
+        !isNumber(cashIncome)
+      );
+      appData.income[itemIncome] = cashIncome;
+    }
+
+    /**
+     * 2) Возможные расходы (addExpenses) вывести строкой в консоль каждое слово с большой буквы слова разделены запятой и пробелом
+loop-splice-join
+
+Пример (Интернет, Такси, Коммунальные расходы)
      */
 
+    let addExpenses = prompt('“Перечислите возможные статьи расходов через запятую”', 'Подарки, путешествия, кУРСы, массаж');
+    appData.addExpenses = addExpenses.toLowerCase().split(', ');
+    console.log('str 75- appData.addExpenses', appData.addExpenses);
+
+    let addExpensesToStr = appData.addExpenses.map(item => {
+      return item.substring(0, 1).toUpperCase() + item.slice(1);
+    });
+    console.log('str 79- addExpensesToStr: ', addExpensesToStr.join(', '));
+
+    appData.addExpenses = addExpensesToStr;
+    console.log('str 83: appData.addExpenses = addExpensesToStr: ', appData.addExpenses = addExpensesToStr);
+
+
+
+    // appData.addExpenses.forEach(item => {
+    //   let result = item.substring(0, 1).toUpperCase() + item.slice(1);
+    //   console.log('str 85', result.join(', '));
+    // });
+
+
+    //let separateWords = appData.addExpenses.map(function(item) {
+    //   return ' ' + item[0].toUpperCase() + item.slice(1).toLowerCase();
+    // });
+    appData.deposit = confirm('“Есть ли у вас депозит в банке?”');
+
+
     for (let i = 0; i < 2; i++) {
-      let expensesResponse;//Переменная для ответа-суммы
-      let expenseName = prompt('“Введите обязательную статью расходов?”');//Переменная с ответом- текстом
+      let expensesResponse;//Переменная для ответа-суммы// cashExpenses у Максима
+
+
+      let expenseName;//Переменная с ответом- текстом, ItemExpenses у Максима 
+      // let expenseName = prompt('“Введите обязательную статью расходов?”');//Переменная с ответом- текстом, ItemExpenses у Максима 
+      do {
+        expenseName = prompt('“Введите обязательную статью расходов?”');
+      } while (
+        expenseName === '' || expenseName === null || isNumber(expenseName) || expenseName.trim() === '' || isNumber(expenseName.trim())
+      );
+
+
       do {
         expensesResponse = prompt('“Во сколько это обойдется?”');
       }
@@ -105,8 +160,26 @@ expenses:
     } else if (appData.budgetDay < 0) {
       return console.log('“Что то пошло не так”');
     }
+  },
+
+  //Urok-8
+  getInfoDeposit: function () {
+    if (appData.deposit) {
+      do {
+        appData.percentDeposit = prompt('Какой годовой процент?', '10');
+      } while (!isNumber(appData.percentDeposit));
+
+      do {
+        appData.moneyDeposit = prompt('Какая сумма заложена?', '10000');
+      } while (!isNumber(appData.moneyDeposit));
+    }
+  },
+
+  calcSavedMoney: function () {//метод для умноженая budgetMonth на period
+    return appData.budgetMonth * appData.period;
   }
 };
+
 
 
 
@@ -117,6 +190,7 @@ appData.getExpensesMonth();
 
 appData.getBudget();
 
+appData.getInfoDeposit();
 
 //('*** ИТОГО: ***');
 console.log('*** Расходы за месяц: ***');
@@ -129,6 +203,11 @@ console.log('*** Уровень дохода: ***');
 appData.getStatusIncome();
 
 
+
+console.log('ПРОВЕРКА calcSavedMoney-', appData.calcSavedMoney());
+console.log('Годовой %-', appData.percentDeposit);
+console.log('Сумма залога-', appData.moneyDeposit);
+
 //13) Используя цикл for in для объекта (appData), вывести в консоль сообщение "Наша программа включает в себя данные: " (вывести все свойства и значения)
 
 console.log('Наша программа включает в себя данные: ');
@@ -137,5 +216,24 @@ for (let key in appData) {
 }
 
 
+/*
+1) Сделать проверку при получении данных:
 
+   - наименование дополнительного источника заработка
+   - сумма дополнительного заработка
+   - ввод статьи обязательных расходов
+   - годовой процент депозита
+   - сумма депозита
+
+
+
+Что значит проверка данных: где должен быть текст там только текст, где цифры только цифры!
+Если проверку не прошло, то переспрашивать!
+
+2) Возможные расходы (addExpenses) вывести строкой в консоль каждое слово с большой буквы слова разделены запятой и пробелом
+loop-splice-join
+
+Пример (Интернет, Такси, Коммунальные расходы)
+
+ */
 
